714cc83156719416c4b766e9ddf08c7a2489e588
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-02-24 21:25:40 +0530

    Add: FibonacciSeries

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        public static int PreviousToPrevious = 0;
+        public static int Previous = 0;
+        public static int Next = 1;
+
+        public static void GetSeries()
+        {
+            Console.WriteLine("Enter the Number to Find Fibonacci Series");
+            int input = Convert.ToInt32(Console.ReadLine());
+            while(Next<input)
+            {
+                PreviousToPrevious = Previous;
+                Previous = Next;
+                Next = PreviousToPrevious + Previous;
+                Console.WriteLine(Previous);
+            }
+        }
+    }
+}
