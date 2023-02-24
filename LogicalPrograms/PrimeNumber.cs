51c342f5b4da563fd991c88b5cb1678874ccf86b
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-02-24 22:02:03 +0530

    Add: PrimeNumber

---
+++ LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PrimeNumber
+    {
+        public static void PrimeCheck()
+        {
+            Console.WriteLine("Enter the number to Check");
+            int input = Convert.ToInt32(Console.ReadLine());
+            bool ifEnteredCheck = false;
+
+            for(int i = 2; i < input; i++)
+            {
+                if(input % i == 0)
+                {
+                    Console.WriteLine("The Number is not a Prime Number");
+                    ifEnteredCheck = true;
+                    break;
+                }
+            }
+            if(ifEnteredCheck == false)
+            {
+                Console.WriteLine("The Number is Prime Number");
+            }
+        }
+
+    }
+}
